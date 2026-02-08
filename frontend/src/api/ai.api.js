8750cdec1aee05238ff3a5860d026329e31d4486
rushi2212 <rushikesh220703@gmail.com>
2026-02-08 21:05:02 +0530

    result add 2

--- frontend/src/api/ai.api.js
+++ frontend/src/api/ai.api.js
@@ -1,11 +1,11 @@
-import api from "./axios";
+import { api2 } from "./axios";
 
 export const prioritizeTasks = async (tasks) => {
-  const response = await api.post("/ai/prioritize", { tasks });
+  const response = await api2.post("/ai/prioritize-tasks", { tasks });
   return response.data;
 };
 
 export const agenticAnalysis = async (tasks) => {
-  const response = await api.post("/ai/agentic", { tasks });
+  const response = await api2.post("/ai/agentic-analysis", { tasks });
   return response.data;
 };
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,10 @@ const api = axios.create({
   // baseURL: "http://localhost:5000/api",
   baseURL: "https://smart-collaboration.onrender.com/api",
 });
-
+const api2 = axios.create({
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-collaboration-fastapi.onrender.com/",
+});
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
@@ -12,3 +15,4 @@ api.interceptors.request.use((req) => {
 });
 
 export default api;
+export { api2 };
