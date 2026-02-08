a127283881c5ac46e187e640094fd52fa7a4624f
rushi2212 <rushikesh220703@gmail.com>
2026-02-08 20:44:28 +0530

    result add

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,9 +1,6 @@
 import { io } from "socket.io-client";
 
-const defaultSocketUrl =
-  typeof window !== "undefined"
-    ? `${window.location.protocol}//${window.location.hostname}:5000`
-    : "http://localhost:5000";
+const defaultSocketUrl = "https://smart-collaboration.onrender.com";
 
 const socket = io(import.meta.env.VITE_SOCKET_URL || defaultSocketUrl, {
   transports: ["websocket", "polling"],
